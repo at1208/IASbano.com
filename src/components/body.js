a96211821b97cc9b83ddc45285684c4019a6add8
at1208 <amantiwari12081999@gmail.com>
2019-12-20 14:34:04 +0530

    almost done

---
+++ src/components/body.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import './body.css'
+import { MdDonutSmall } from "react-icons/md";
+
+
+
+ const Body = () => {
+   return <div className='col-md-12 container b5'>
+   <div className='row col justify-content-center'>
+
+<div className='card col-md-3 text-center b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='https://manavrachna.edu.in/wp-content/uploads/2017/08/Strategy-and-books-for-early-birds-to-crack-the-IAS-exam-1024x585.jpg' className='b1'/>
+ <h6 className='b6'>STUDY TIPS</h6>
+</div>
+
+<div className='card col-md-3 text-center  b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='https://m.economictimes.com/thumb/msid-70508967,width-1200,height-900,resizemode-4,imgsize-222459/article-370-bccl.jpg' className='b1'/>
+ <h6 className='b6'>ARTICLE 370</h6>
+</div>
+
+<div className='card col-md-3 text-center b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='https://bsmedia.business-standard.com/_media/bs/img/misc/2019-08/05/full/articles-35a-and-370-1564979741-52742190.jpg'  className='b1'/>
+ <h6 className='b6'>ARTICLE 35A</h6>
+</div>
+
+<div className='card col-md-3 text-center b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='https://i.pinimg.com/originals/10/8c/9e/108c9e24f6bee49f5a09eb39b3521afe.png'  className='b1'/>
+ <h6 className='b6'>CONSTITUTION OF INDIA</h6>
+</div>
+
+<div className='card col-md-3 text-center b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='https://st1.latestly.com/wp-content/uploads/2018/04/Latestly-India-2.jpg'  className='b1'/>
+ <h6 className='b6'>INDIA</h6>
+</div>
+
+<div className='card col-md-3 text-center b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='http://assets-news-bcdn-ll.dailyhunt.in/cmd/resize/400x400_60/fetchdata15/images/6c/5b/b1/6c5bb166509db5c8602e48ae3ed68b23.jpg'  className='b1'/>
+ <h6 className='b6'>CAB</h6>
+</div>
+
+<div className='card col-md-3 text-center b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='https://i1.wp.com/blog.scconline.com/wp-content/uploads/2017/09/Supreme-Court_Colour.jpg?resize=886%2C590&ssl=1'  className='b1'/>
+ <h6 className='b6'>SUPREME COURT OF INDIA</h6>
+</div>
+
+<div className='card col-md-3 text-center b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='https://manavrachna.edu.in/wp-content/uploads/2017/08/Strategy-and-books-for-early-birds-to-crack-the-IAS-exam-1024x585.jpg'  className='b1'/>
+ <h6 className='b6'>TOP 10 STUDY STRATEGY</h6>
+</div>
+
+<div className='card col-md-3 text-center b3'>
+<div className='b2'>
+<MdDonutSmall />
+</div>
+ <img src='http://assets-news-bcdn-ll.dailyhunt.in/cmd/resize/400x400_60/fetchdata15/images/6c/5b/b1/6c5bb166509db5c8602e48ae3ed68b23.jpg'  className='b1'/>
+ <h6 className='b6'>TOP 10 STUDY STRATEGY</h6>
+</div>
+   </div>
+   </div>
+ }
+export default Body;
---
+++ src/components/header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './header.css'
+ import { Carousel } from 'antd';
+ import 'antd/dist/antd.css';
+import SearchBar from './serchbar'
+
+const Header  = () => {
+  return <div className='c1'>
+  <div className='a1 container-fluid'>
+<div className='text-center a8'><i class="fa fa-graduation-cap" style={{ "margin": "7px"}}></i>IAS<span className='a7'>Bano</span>
+<button className='float-right btn-sm btn-info a2'>Login</button>
+<button className='float-right btn-sm btn-info a2 '>Page3</button>
+<button className='float-right btn-sm btn-info a2'>Page2</button>
+<button className='float-right btn-sm btn-info a2'>Page1</button>
+</div>
+  </div>
+
+
+  <div id="carouselExampleFade" className="carousel slide carousel-fade a4" data-ride="carousel">
+  <div className="carousel-inner a6">
+    <div className="carousel-item active">
+      <img src="https://photoartinc.com/wp-content/uploads/2018/02/inspiring-cover-photos-5.jpg " class="d-block w-100" alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src=" https://i.pinimg.com/originals/b2/34/61/b2346113cdc5873d9a55c227d263bd7f.jpg" class="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+</div>
+    <div className='b7 float-right'>
+  <SearchBar />
+  </div>
+  </div>
+}
+export default Header;
